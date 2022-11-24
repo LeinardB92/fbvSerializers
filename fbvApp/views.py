e9ef26ac078824bd0ad8746439c56de142fd7143
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2022-11-24 17:39:07 -0600

    Se agregaron comentarios a view.py

--- fbvApp/views.py
+++ fbvApp/views.py
@@ -5,6 +5,7 @@ from fbvApp.models import Student
 from fbvApp.serializers import StudentSerializer
 # 'Response', devuelve contenido que se puede representar en varios tipos de contenido, según la solicitud del cliente también podrías usar HttpResponse, pero 'Response'. El uso de la clase Response simplemente proporciona una interfaz más agradable para devolver respuestas de API web, que se pueden representar en múltiples formatos..
 from rest_framework.response import Response
+# Ayuda a describir el estado de los códigos HTTP, para mejorar la comprensión del código. 
 from rest_framework import status
 from rest_framework.decorators import api_view
 
@@ -13,13 +14,17 @@ from rest_framework.decorators import api_view
 def student_list(request):
 
     if request.method =='GET':
+        # Obtenemos un queryset de la base de datos 
         students = Student.objects.all()
-        serializer=StudentSerializer(students,many=True)
+        # Serializamos el queryset 
+        serializer = StudentSerializer(students,many=True)
         return Response(serializer.data)
 
     elif request.method == 'POST':
+        # Deserializamos los datos mandados por el usuario a través de 'request', serializer es ahora un objeto python que podemos utilizar.
         serializer = StudentSerializer(data=request.data)
         if serializer.is_valid():
+            # Guardamos en la base de datos
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
